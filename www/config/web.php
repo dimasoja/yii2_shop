6d70bc04c7607ebecaded8ec4c0ce25b2eb7c25d
Dima Soya <dimasoja@ukr.net>
2020-05-23 19:38:45 +0300

    creates a URL

--- www/config/web.php
+++ www/config/web.php
@@ -15,6 +15,7 @@ $config = [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
             'cookieValidationKey' => 'L90I6RlUnqTJsDNwnVgSO070LRqN4njH',
+            'baseUrl' => '',
         ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
@@ -43,14 +44,15 @@ $config = [
             ],
         ],
         'db' => $db,
-        /*
+        
         'urlManager' => [
             'enablePrettyUrl' => true,
+            'enableStrictParsing' => false,
             'showScriptName' => false,
             'rules' => [
             ],
         ],
-        */
+        
     ],
     'params' => $params,
 ];
